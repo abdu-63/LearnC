1ecb5c5a6c42cd35305762da50acbdf210a4bea0
Abdu <molepatate@gmail.com>
2025-10-29 19:07:53 +0100

    boucle for en while

---
+++ 814.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main(void) {
+  /*
+  for(int i = 5; i < 15; i++) {
+    printf("%d\n", i);
+  }
+  */
+  int n=5;
+  while (n<15)
+    printf("%d\n", n);
+  return 0;
+}
