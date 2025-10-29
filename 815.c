e25cc28b1849e1010e1b3ddf20fff98bbf7f0148
Abdu <molepatate@gmail.com>
2025-10-29 19:14:04 +0100

    boucle do ... while en boucle while

---
+++ 815.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main(void) {
+  /*
+  int i = 1;
+  do {
+  printf("%d\n",i++);
+  } while (i <= 5);
+  */
+
+  int i = 1;
+  while (i <= 5) {
+    printf("%d\n",i++);
+  }
+  return 0;
+}
